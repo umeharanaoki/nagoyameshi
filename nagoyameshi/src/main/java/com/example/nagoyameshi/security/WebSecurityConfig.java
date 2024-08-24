aa39fda847a433186131ea6e63ef5611f5f3495d
N.umehara <n6773938@gmail.com>
2024-08-24 14:01:46 +0900

    Revert "test"
    This reverts commit 670a21ff777d3eeb3a8ad2b79679125673ab7e7d.

--- nagoyameshi/src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
+++ nagoyameshi/src/main/java/com/example/nagoyameshi/security/WebSecurityConfig.java
@@ -25,22 +25,12 @@ public class WebSecurityConfig {
 				.anyRequest().authenticated() // 上記以外のURLはログインが必要（会員または管理者のどちらでもOK）
 			)
 			.formLogin((form) -> form
-			    .loginPage("/login")
-			    .loginProcessingUrl("/login")
-			    .successHandler((request, response, authentication) -> {
-			        // カスタムロジックを追加してリダイレクト先を決定
-			        response.sendRedirect("/?loggedIn");
-			    })
-			    .failureUrl("/login?error")
-			    .permitAll()
+				.loginPage("/login")			 // ログインページのURL
+				.loginProcessingUrl("/login")	 // ログインフォームの送信先URL
+				.defaultSuccessUrl("/?loggedIn") // ログイン成功時のリダイレクト先URL
+				.failureUrl("/login?error")		 // ログイン失敗時のリダイレクト先URL
+				.permitAll()
 			)
-//			.formLogin((form) -> form
-//				.loginPage("/login")			 // ログインページのURL
-//				.loginProcessingUrl("/login")	 // ログインフォームの送信先URL
-//				.defaultSuccessUrl("/?loggedIn") // ログイン成功時のリダイレクト先URL
-//				.failureUrl("/login?error")		 // ログイン失敗時のリダイレクト先URL
-//				.permitAll()
-//			)
 			.logout((logout) -> logout
 				.logoutSuccessUrl("/?loggedOut") // ログアウト時のリダイレクト先URL
 				.permitAll()
